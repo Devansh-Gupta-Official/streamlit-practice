ff24a5bf4e6baee8a1243b3af93a940c0afec2d3
Devansh-Gupta-Official <devanshcool.gupta@gmail.com>
2024-02-20 17:57:48 +0530

    displaying text in a streamlit app

--- demo.py
+++ demo.py
@@ -1,3 +1,35 @@
 import streamlit as st
 
-st.title("Practicing Streamlit")
+st.title("Practicing Streamlit")     #title
+
+st.header("Header")      #header
+st.subheader("Sub Header")      #subheader
+
+st.text("This is my first time learning and using Streamlit")      #text
+
+st.markdown("""# h1 tag
+### h3 tag
+:moon: <br>
+:sunglasses:
+**bold**
+_italics_
+""",True)      #True implies to the markdown that<br> is not a text but a html command
+
+st.latex(r'''a + ar + a r^2 + a r^3 + \cdots + a r^{n-1} =
+     \sum_{k=0}^{n-1} ar^k =
+     a \left(\frac{1-r^{n}}{1-r}\right)''')                  #using latex
+
+
+#write command  IMPORTANT*****
+st.write("devansh","streamlit","practice")
+st.write("# devansh")
+st.write(st)
+st.write(sum)
+
+
+d ={
+    "name":"Harsh",
+    "language":"Python",
+    "topic":"Streamlit"
+} 
+st.write(d)   #passing a dictionary
